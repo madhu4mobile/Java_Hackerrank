b08d829d316324d42796fe3aa80afd91a900b846
u332678 <u332678@L5CG535081R.gac.gulfaero.com>
2018-07-11 16:20:37 -0400

    after correcting package names

--- loopsI_MathTable/MathTable.java
+++ loopsI_MathTable/MathTable.java
@@ -15,6 +15,7 @@ public class MathTable {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+    	System.out.print("Math table for Which number? :");
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
         scanner.close();
